ecf1ea510e4dcce7a5ec177647ac2e7ff4aefe28
Bong Nguyen <bongtham01@gmail.com>
2025-01-09 21:25:52 +0700

    Reset main branch

--- SourceCode/MobilePhoneSalesManagement/ServerApp.BLL/Services/ViewModels/Authentication/AuthResultVm.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServerApp.BLL.Services.ViewModels.Authentication
-{
-    public class AuthResultVm
-    {
-        public string Token { get; set; }
-        public string RefreshToken { get; set; }
-        public DateTime ExpiresAt { get; set; }
-    }
-}
