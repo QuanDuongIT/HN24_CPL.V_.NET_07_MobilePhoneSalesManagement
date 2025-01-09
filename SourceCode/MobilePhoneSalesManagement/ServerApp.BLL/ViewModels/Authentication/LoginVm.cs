78d3f043f9fd72217eb9546c719f31187ac73f41
Duong <tieuduong.25.1.98@gmail.com>
2025-01-09 14:38:02 +0700

    merge branch

--- SourceCode/MobilePhoneSalesManagement/ServerApp.BLL/ViewModels/Authentication/LoginVm.cs
+++ SourceCode/MobilePhoneSalesManagement/ServerApp.BLL/ViewModels/Authentication/LoginVm.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace ServerApp.BLL.Services.ViewModels.Authentication
+namespace ServerApp.PL.ViewModels.Authentication
 {
     public class LoginVm
     {
