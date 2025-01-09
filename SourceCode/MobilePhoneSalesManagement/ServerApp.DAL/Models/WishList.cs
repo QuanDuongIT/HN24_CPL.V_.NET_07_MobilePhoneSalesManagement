2342abd919bde8f7d8e01b3e6828a3bea8bf06bd
Bong Nguyen <bongtham01@gmail.com>
2025-01-09 15:20:49 +0700

    bong

--- SourceCode/MobilePhoneSalesManagement/ServerApp.DAL/Models/WishList.cs
+++ SourceCode/MobilePhoneSalesManagement/ServerApp.DAL/Models/WishList.cs
@@ -2,7 +2,6 @@
 {
     public class WishList
     {
-        public int WishListId {get; set;}
         public int UserId { get; set; }
         public int ProductId { get; set; }
         public DateTime AddedAt { get; set; } = DateTime.Now;
