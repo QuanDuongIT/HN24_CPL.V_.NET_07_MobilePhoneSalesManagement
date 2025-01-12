131d88679520107ae7cceb22b015ea3017490be7
Bong Nguyen <bongtham01@gmail.com>
2025-01-12 14:37:20 +0700

    update frontend base

--- SourceCode/MobilePhoneSalesManagement_Frontend/src/app/app.routes.ts
+++ SourceCode/MobilePhoneSalesManagement_Frontend/src/app/app.routes.ts
@@ -4,18 +4,30 @@ import { ProductManagementComponent } from './features/admin/product-management/
 import { BrandManagementComponent } from './features/admin/brand-management/brand-management.component';
 import { UserManagementComponent } from './features/admin/user-management/user-management.component';
 import { AdminLayoutComponent } from './features/admin/admin-layout/admin-layout.component';
+import { ClientLayoutComponent } from './features/client/client-layout/client-layout.component';
+import { HomeComponent } from './features/client/home/home.component';
 
 export const routes: Routes = [
-    { path: 'admin', redirectTo: '/admin/user-management', pathMatch: 'full' },
-    {
-        path: 'admin',
-        component: AdminLayoutComponent,
-        children: [
-          { path: 'user-management', component: UserManagementComponent },
-          { path: 'brand-management', component: BrandManagementComponent },
-          { path: 'product-management', component: ProductManagementComponent },
-          { path: 'order-management', component: OrderManagementComponent },
-        ]
-      }
+  { path: 'admin', redirectTo: '/admin/user-management', pathMatch: 'full' },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      { path: 'user-management', component: UserManagementComponent },
+      { path: 'brand-management', component: BrandManagementComponent },
+      { path: 'product-management', component: ProductManagementComponent },
+      { path: 'order-management', component: OrderManagementComponent },
+    ],
+  },
+  {
+    path: '',
+    component: ClientLayoutComponent, // Giao diện layout cho client
+    children: [
+      { path: 'home', component: HomeComponent }, // Trang chủ của client
+      // { path: 'products', component: ProductListComponent }, // Danh sách sản phẩm
+      // { path: 'product/:id', component: ProductDetailComponent }, // Chi tiết sản phẩm
+      // { path: 'cart', component: CartComponent }, // Giỏ hàng
+      // { path: 'checkout', component: CheckoutComponent }, // Thanh toán
+    ]
+  }
 ];
-
