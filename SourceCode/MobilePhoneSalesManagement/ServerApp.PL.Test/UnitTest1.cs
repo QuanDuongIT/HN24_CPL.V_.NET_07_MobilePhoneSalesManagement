931ab2a97dfe0e1e3a3184d1e1540f8589524b35
Duong <tieuduong.25.1.98@gmail.com>
2025-01-19 19:17:17 +0700

    NUnit test

---
+++ SourceCode/MobilePhoneSalesManagement/ServerApp.PL.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ServerApp.PL.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
