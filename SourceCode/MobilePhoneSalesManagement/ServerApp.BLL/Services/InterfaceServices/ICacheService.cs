e78ad5e4179a30b16c73c96446340f2f28508c3d
cho <thaochuoi159mla@gmail.com>
2025-01-20 16:52:22 +0700

    commit haha

--- SourceCode/MobilePhoneSalesManagement/ServerApp.BLL/Services/InterfaceServices/ICacheService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServerApp.BLL.Services.InterfaceServices
-{
-    public interface ICacheService
-    {
-        Task SetAsync<T>(string key, T value, TimeSpan expiration);
-        Task<T> GetAsync<T>(string key);
-        Task RemoveAsync(string key);
-    }
-}
--- SourceCode/MobilePhoneSalesManagement/ServerApp.BLL/Services/InterfaceServices/IEmailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ServerApp.BLL.Services.InterfaceServices
-{
-    public interface IEmailService
-    {
-        Task SendAsync(string to, string subject, string body);
-    }
-}
